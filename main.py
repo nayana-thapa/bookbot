10bd7df705cebcd2a7b29c7980da09cadec5ca25
Nayana Thapa <thapanayana02@gmail.com>
2023-03-13 01:04:39 +0530

    completed the functionalities of book bot

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
-    print(text)
     count_text = get_count(text)
-    print(count_text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_text} words found in the document")
+
+    chars_in_dict = get_characters_dict(text)
+    sorted_char_list = get_sorted_list(chars_in_dict)
+
+    for item in sorted_char_list:
+        if item["char"].isalpha():
+            print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
 
@@ -23,7 +32,29 @@ def get_count(text):
     # Example: "I am a student" -> ["I", "am", "a", "student"]
     list_of_word = text.split() 
     return len(list_of_word)
- 
+
+def get_characters_dict(texts):
+    dict = {}
+    for letter in texts:
+        
+        char = letter.lower()
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
+
+def sort_on(d):
+    return d["num"]
+
+def get_sorted_list(dict):
+    list = []
+    for ch in dict:
+        list.append({"char": ch, "num": dict[ch]})
+    list.sort(reverse=True, key=sort_on)
+    return list
+
+
 main()
 
 
